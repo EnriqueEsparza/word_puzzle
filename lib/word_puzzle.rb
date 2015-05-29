2fb26157fd4edb006128b921ebdb738633377537
enrique <iqueart@yahoo.com>
2015-05-29 11:02:02 -0700

    fail initial integration spec, routing has not been setup

--- lib/word_puzzle.rb
+++ lib/word_puzzle.rb
@@ -2,13 +2,9 @@ class String
   define_method(:puzzle) do
     sentence = []
     words = self.split
-
     words.each() do |word|
-
       letters = word.split("")
-
       letters.each() do |letter|
-
         if letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u" || letter == "A" || letter == "E" || letter == "I" || letter == "O" || letter == "U"
           letter.replace("-")
         end
@@ -16,7 +12,5 @@ class String
       sentence.push(letters.join())
     end
     sentence.join(" ")
-
-
   end
 end
