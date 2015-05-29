055a105927a5f78dfdec3f1e9e83414f7cdd46d7
enrique <iqueart@yahoo.com>
2015-05-29 11:13:14 -0700

    passed integration test

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ require('sinatra/reloader')
 require('./lib/word_puzzle')
 also_reload('lib/**/*.rb')
 
-# get('/') do
-#   erb(:index)
-# end
+get('/') do
+  erb(:index)
+end
+
+get('/results') do
+  sentence = params.fetch('sentence')
+  @puzzled_string = sentence.puzzle
+  erb(:results)
+end
